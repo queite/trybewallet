5c01620b197631254ab476dfd1d9f5c1499b5f49
Queite <queitesc@gmail.com>
2022-04-03 16:47:33 -0300

    Updates

---
+++ precache-manifest.2420b62c556b0a9779a2296d5c1e9548.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614c65638025610dc9372a13ee542b63",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "a1c2ad0fc292aa41a547",
+    "url": "/trybewallet/static/css/main.e3eb5cbf.chunk.css"
+  },
+  {
+    "revision": "06b5faefe51cc3b434cf",
+    "url": "/trybewallet/static/js/2.b48de650.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.b48de650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1c2ad0fc292aa41a547",
+    "url": "/trybewallet/static/js/main.1c766b19.chunk.js"
+  },
+  {
+    "revision": "efdc6867495475ae3922",
+    "url": "/trybewallet/static/js/runtime-main.daf72ee0.js"
+  }
+]);
